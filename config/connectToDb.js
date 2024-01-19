e13b04726ed9a94ff0c1000f990ef930d85172ea
walegithub2023 <ogundejiwaletemitope@gmail.com>
2024-01-19 18:24:14 +0100

    made config changes

--- config/connectToDb.js
+++ config/connectToDb.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 //connect to database
 async function connectToDb() {
   try {
-    await mongoose.connect(process.env.DB_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to nafa database");
   } catch (err) {
     console.log(err);
